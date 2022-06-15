2ecc6f8704099c3bc1cb5153cbca28982722b905
Alexander Stenberg <alstenberg@live.se>
2022-06-15 11:11:40 +0200

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar-container">
+      <h2 className="navbar-header">Alexander Stenberg</h2>
+      <ul className="navbar-links">
+        <li className="navbar-link">Hem</li>
+        <li className="navbar-link">Kontakt</li>
+        <li className="navbar-link">Om Mig</li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import Navbar from '../Components/Navbar';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+}
