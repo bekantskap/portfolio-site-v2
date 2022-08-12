f246540ec81792ec9d1ab3c500424133ba391de9
Alexander Stenberg <alstenberg@live.se>
2022-08-12 12:04:45 +0200

    added scroll animation on info section

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Image from '../img/avatar.png';
 
 export default function Info() {
+  const [slideIn, setSlideIn] = useState(false);
+
+  useEffect(() => {
+    slideOnScrolltest();
+    window.addEventListener('scroll', slideOnScrolltest);
+    console.log(slideIn);
+  });
+
+  const slideOnScrolltest = () => {
+    if (window.scrollY >= 600) {
+      setSlideIn(true);
+    } else {
+      setSlideIn(false);
+    }
+  };
+
   return (
     <main className="info-container">
       <section className="info-content">
-        <img className="info-box__image" src={Image} alt="portrait" />
+        <img
+          className={`info-box__image ${
+            slideIn ? 'hide-slide' : 'slide-in__left'
+          } `}
+          src={Image}
+          alt="portrait"
+        />
       </section>
       <div className="info-line"></div>
-      <section className="info-content__right">
+      <section
+        className={`info-content__right ${
+          slideIn ? 'hide-right' : 'slide-in__right'
+        } `}
+      >
+        <div className="info-box__bio">
+          <h3>BIO</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium modi repellat provident labore? Quidem, fugiat. Mollitia
+            dolore maiores beatae sapiente optio totam reiciendis excepturi
+            perspiciatis at aspernatur est, unde amet?
+          </p>
+        </div>
         <div className="info-box">
           <div className="info-box__content">
             <div>
@@ -16,11 +53,11 @@ export default function Info() {
               <i class="fa-solid fa-face-grin"></i>
               <i class="fa-solid fa-face-grin-tears"></i>
             </div>
-            <div>
+            {/* <div>
               <h3>..när koden inte funkar</h3>
               <i class="fa-solid fa-face-meh-blank"></i>
               <i class="fa-solid fa-face-grin-beam-sweat"></i>
-            </div>
+            </div> */}
             <div>
               <h3>Hobby</h3>
               <i class="fa-solid fa-futbol"></i>
@@ -40,15 +77,6 @@ export default function Info() {
               <i class="fa-brands fa-react"></i>
             </div>
           </div>
-        </div>
-        <div className="info-box__bio">
-          <h3>BIO</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Praesentium modi repellat provident labore? Quidem, fugiat. Mollitia
-            dolore maiores beatae sapiente optio totam reiciendis excepturi
-            perspiciatis at aspernatur est, unde amet?
-          </p>
         </div>
       </section>
     </main>
